59421f23d4610b1c7753c4dabbd11a9f0f7de767
Charity553 <kokicharity3@gmail.com>
2023-07-02 12:19:18 +0300

    adding scripts in nextjs

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,24 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
-  );
-}
+    return (
+      <>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">← Back to home</Link>
+        </h2>
+      </>
+    );
+  }
