2beae308f2cd052c99c9d36be9390a2fe5f4a987
Charity553 <kokicharity3@gmail.com>
2023-07-03 10:04:59 +0300

    CSS styling

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+// `pages/_app.js`
+import '../styles/globals.css';
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,24 +1,20 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
+//import Script from 'next/script';
+// import the css file for styling
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-      <>
+        <Layout>
         <Head>
           <title>First Post</title>
         </Head>
-        <Script
-          src="https://connect.facebook.net/en_US/sdk.js"
-          strategy="lazyOnload"
-          onLoad={() =>
-            console.log(`script loaded correctly, window.FB has been populated`)
-          }
-        />
+    
         <h1>First Post</h1>
         <h2>
-          <Link href="/">← Back to home</Link>
+          <Link href="/">Back to home</Link>
         </h2>
-      </>
+      </Layout>
     );
   }
